1c6a7bfa05fa78aea29c860f0474aabd673015c4
Bryan Fortin <bryanf42@hotmail.com>
2019-06-27 13:52:52 -0400

    Added the profile dropdown menu

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -11,8 +11,12 @@ class Header extends Component {
             <li>
               <Link to="/">Nouvel USage</Link>
             </li>
-            <li className="profile-menu">
-              <Link to="/MyProfile"><i className="material-icons">account_circle</i></Link>
+            <li className="profile-dropdown">
+              <a className="profile-dropdown-button"><i className="material-icons">account_circle</i></a>
+              <div class="profile-dropdown-content">
+                <Link to="/MyProfile" className="profile-dropdown-element">Mon profil</Link>
+                <Link to="/MySales" className="profile-dropdown-element">Mes annonces</Link>
+              </div>
             </li>
           </ul>
         </div>
