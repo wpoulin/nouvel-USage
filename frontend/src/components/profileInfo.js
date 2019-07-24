df42c89c3e5e59d66107defdd9c4448141cf3457
Chapeau <guillaume.auclair@usherbrooke.ca>
2019-07-24 17:16:08 -0400

    Update profileInfo.js

--- frontend/src/components/profileInfo.js
+++ frontend/src/components/profileInfo.js
@@ -7,15 +7,29 @@ class ProfileInfo extends Component {
   constructor(props) {
     super(props);
     // N’appelez pas `this.setState()` ici !
+    console.log(this.props)
     this.state = { 
+      firstName : "",
+      lastName : "",
+      username : "",
+      email : "",
+      tel : "",
+      cell : "",
+      city : ""
+     };
+  }
+
+  // eslint-disable-next-line react/no-deprecated
+  componentWillReceiveProps(props){
+    this.setState({       
       firstName : props.firstName,
       lastName : props.lastName,
       username : props.username,
       email : props.email,
       tel : props.lastName,
       cell : props.cell,
-      city : props.city
-     };
+      city : props.city 
+    })
   }
   render() {
     return (
@@ -23,19 +37,19 @@ class ProfileInfo extends Component {
             <article>
                 <form>
                   <label className="profile-label">Prénom</label>
-                  <input className="profile-input" value={this.props.firstName} onChange={e => this.setState({ firstName: e.target.value })}></input>
+                  <input className="profile-input" type="text" value={this.state.firstName} onChange={e => this.setState({ firstName: e.target.value })}></input>
                   <label className="profile-label">Nom</label>
-                  <input className="profile-input" value={this.props.lastName}></input>
+                  <input className="profile-input" value={this.state.lastName} onChange={e => this.setState({ lastName: e.target.value })}></input>
                   <label className="profile-label">Nom d'utilisateur</label>
-                  <input className="profile-input" value={this.props.username}></input>
+                  <input className="profile-input" value={this.state.username} onChange={e => this.setState({ username: e.target.value })}></input>
                   <label className="profile-label">Courriel</label>
-                  <input className="profile-input" value={this.props.email}></input>
+                  <input className="profile-input" value={this.state.email} onChange={e => this.setState({ email: e.target.value })}></input>
                   <label className="profile-label">Téléphone</label>
-                  <input className="profile-input" value={this.props.tel}></input>
+                  <input className="profile-input" value={this.state.tel} onChange={e => this.setState({ tel: e.target.value })}></input>
                   <label className="profile-label">Cellulaire</label>
-                  <input className="profile-input" value={this.props.cell}></input>
+                  <input className="profile-input" value={this.state.cell} onChange={e => this.setState({ cell: e.target.value })}></input>
                   <label className="profile-label">Ville</label>
-                  <input className="profile-input" value={this.props.city}></input>
+                  <input className="profile-input" value={this.state.city} onChange={e => this.setState({ city: e.target.value })}></input>
 
                   <button className="profile-apply-button">Appliquer</button>
                   <Link to="/ResetPassword" className="profile-pw-reset">Réinitialiser le mot de passe</Link>
